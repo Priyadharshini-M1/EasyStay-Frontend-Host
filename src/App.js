43fc8fa9a205912e913154847e97e207f29dc9d6
Priyadharshini <priya@Priyadharshini>
2024-05-21 10:02:29 +0530

    update package adn app commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Login';
@@ -5,22 +6,25 @@ import LoginForm from './LoginForm';
 import Signup from './Signup';
 import Home from './Home';
 import Book from './Book';
+import axios from 'axios';
 
+// Set default base URL and headers for axios
 axios.defaults.baseURL = "https://2972-16-171-12-189.ngrok-free.app";
 axios.defaults.headers = {
-  "Content-Type" : "application/json","ngrok-skip-browser-warning":"69420",
+  "Content-Type": "application/json",
+  "ngrok-skip-browser-warning": "69420",
 };
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element = {<Login/>}/>
-      <Route path='/LoginForm' element={<LoginForm/>}/>
-      <Route path='/Signup' element={<Signup/>}/>
-      <Route path='/Home' element={<Home/>}/>
-      <Route path='/Book' element={<Book/>}/>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/LoginForm' element={<LoginForm />} />
+        <Route path='/Signup' element={<Signup />} />
+        <Route path='/Home' element={<Home />} />
+        <Route path='/Book' element={<Book />} />
+      </Routes>
     </BrowserRouter>
   );
 }
